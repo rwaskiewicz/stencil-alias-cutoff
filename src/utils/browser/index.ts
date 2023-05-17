b30aec92a3377649dbdaf207a54e24a804757c81
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2023-05-17 10:19:40 -0400

    first attempt at a repro

---
+++ src/utils/browser/index.ts
@@ -0,0 +1,3 @@
+export const win: Window | undefined = typeof window !== 'undefined' ? window : undefined;
+
+export const doc: Document | undefined = typeof document !== 'undefined' ? document : undefined;
